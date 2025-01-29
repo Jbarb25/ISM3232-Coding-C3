b1505a64ca1bb13dd6ec67d908fcc83ad1a91438
Jbarb25 <barboza205@usf.edu>
2025-01-29 17:01:02 -0500

    Task 1 - Product Price Management
    Declaring the array of prices, adding a new price to the end of the array, and removing the first price of the array.

---
+++ cc_3.js
@@ -0,0 +1,7 @@
+//Task 1: Product Price Management Scenario: Online Store (Product Pricing)
+let product_prices = [4.99, 3.50, 5.75, 8.25, 9.99]
+
+    //Task 1: Add price to the end of the list and remove the first
+product_prices.push(7.50)
+product_prices.shift()
+console.log(product_prices)
