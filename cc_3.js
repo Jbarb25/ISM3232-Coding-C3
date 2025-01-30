eaac30b7bebe28fbf04b1f07f21e2ec3989159f2
Jbarb25 <barboza205@usf.edu>
2025-01-30 15:30:12 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -5,3 +5,17 @@ let product_prices = [4.99, 3.50, 5.75, 8.25, 9.99]
 product_prices.push(7.50)
 product_prices.shift()
 console.log(product_prices)
+
+
+
+//Task 2: Modifying Customer Orders Scenario: Order List (Customer Orders)
+let order_list_qty = [3, 4, 2, 1, 6]
+
+    //Task 2: Increase third order by 5
+order_list_qty[2] = order_list_qty[2] + 5
+    //Task 2: Calculate the total number of all orders
+let total_qty = order_list_qty.reduce((sum, order_list_qty) => sum + order_list_qty, 0)
+
+console.log(order_list_qty)
+console.log(total_qty)
+
